79117b39eb09dd9341c6858d66428e74d3db4ede
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-05-01 11:11:33 +0800

    Initial commit

---
+++ resources/views/frontend/user/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('frontend.layouts.app')
+
+@section('content')
+    <div class="row mb-4">
+        <div class="col">
+
+        </div><!-- row -->
+    </div><!-- row -->
+@endsection
+
+@push('after-scripts')
+    @include('frontend.user.dashboard.script')
+@endpush
