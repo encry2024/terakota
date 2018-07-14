585e4401c4feb40b369f510c0f713b45079107b1
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-07-14 17:39:52 +0800

    Incorrect migration. Added change()

--- database/migrations/2018_07_14_173650_make_shift_id_nullable.php
+++ database/migrations/2018_07_14_173650_make_shift_id_nullable.php
@@ -14,7 +14,7 @@ class MakeShiftIdNullable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('shift_id')->nullable();
+            $table->integer('shift_id')->nullable()->change();
         });
     }
 
